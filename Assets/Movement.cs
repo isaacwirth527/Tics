cbd0868b8d51f6ed6edd9f63ca993efaf19f9ac9
isaacwirth527 <49846299+isaacwirth527@users.noreply.github.com>
2019-09-19 15:52:35 -0400

    Started to build

---
+++ Assets/Movement.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Rigidbody rb;
+    public float speed;
+    public float rotationSpeed;
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void FixedUpdate()
+    {
+        if (Input.GetAxis("Vertical") < 0)
+        {
+           rb.velocity = transform.forward * speed * Time.deltaTime;
+        }
+        if (Input.GetAxis("Vertical") > 0)
+        {
+            rb.velocity = -transform.forward * speed * Time.deltaTime;
+        }
+        if (Input.GetAxis("Horizontal") > 0)
+        {
+            transform.Rotate(0, rotationSpeed, 0);
+        }
+        if (Input.GetAxis("Horizontal") < 0)
+        {
+            transform.Rotate(0, -rotationSpeed, 0);
+        }
+        if(Input.GetAxis("Horizontal") == 0 && Input.GetAxis("Vertical") == 0)
+        {
+            rb.velocity = Vector3.zero;
+        }
+    }
+}
+
