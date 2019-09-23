dc103ebd1c32d5cc0ddfe0033e2631c4859eb496
isaacwirth527 <49846299+isaacwirth527@users.noreply.github.com>
2019-09-23 19:38:01 -0400

    BACKING UP BEFORE I BREAK IT LMAO

---
+++ Assets/PeopleSpawn.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PeopleSpawn : MonoBehaviour
+{
+    GameObject person;
+    GameObject[] people;
+    public int numOfPeople;
+    // Start is called before the first frame update
+    void Start()
+    {
+        people = new GameObject[numOfPeople];
+        for (int i = 0; i < numOfPeople; i++)
+        {
+            people[i] = Instantiate(person);
+            people[i].transform.position = new Vector3(Random.Range(-1, -2), 0.99f, Random.Range(0, 20));
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
