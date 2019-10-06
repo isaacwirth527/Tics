0837704cade837440434d477f6ba67a323a81432
isaacwirth527 <49846299+isaacwirth527@users.noreply.github.com>
2019-10-06 15:33:50 -0400

    started working on dialogue system!

---
+++ Assets/PlayerRaycast.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerRaycast : MonoBehaviour
+{
+    public TextMesh speechText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Ray playerRay = new Ray(transform.position, transform.forward);
+        float maxRayDistance = 1f; // just a bit in front
+        Debug.DrawRay(playerRay.origin, playerRay.direction * maxRayDistance, Color.red);
+        RaycastHit raycastHit = new RaycastHit();
+        if (Physics.Raycast(playerRay, maxRayDistance))
+        {
+            if(raycastHit.transform.tag == "dialoguePlayer")
+            {
+                raycastHit.collider.gameObject.GetComponent<dialogueScript>();
+                string displayText = dialogueScript.speech;
+                speechText.text = displayText;
+            }
+            
+        }
+    }
+}
+
